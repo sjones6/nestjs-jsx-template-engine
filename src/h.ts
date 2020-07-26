3da573f1ed0e91f0e67d2cacda8879fc161d70d8
sjones6 <jones.spencera@gmail.com>
2020-07-25 22:35:27 -0400

    correct bug in jsx implementation with arrays

--- src/h.ts
+++ src/h.ts
@@ -7,15 +7,14 @@ function stringifyChildren(child: any): string {
     }
     if (Array.isArray(child)) {
       stringifiedChild = child.join('\n')
-    }
-  
-    // there's a value that exposes a `toString` coercion method
-    if (child && typeof child.toString === 'function') {
+    } else if (child && typeof child.toString === 'function') {
+      // there's a value that exposes a `toString` coercion method
       stringifiedChild = child.toString();
     }
   
-    // coerce a number of falsey varietes to a string
-    if (!child) {
+    // coerce a number of falsey varietes to a string, but exclude null,
+    // which is the standard behavior of React's JSX engine
+    if (!child && child !== null) {
       stringifiedChild = `${child}`;
     }
   }
